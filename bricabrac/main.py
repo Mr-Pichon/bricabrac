366c760868ecf7b9dad33a80d8d640cba9ad3d2a
Pierre Gillet <prrgillet@gmail.com>
2022-09-08 15:22:38 +0200

    [WIP] creation d'une UI

--- bricabrac/main.py
+++ bricabrac/main.py
@@ -3,6 +3,10 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import sys
+from PyQt5 import QtWidgets
+
+from main_window import Ui_MainWindow
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +16,13 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    win = Ui_MainWindow()
+    win.show()
+
+    sys.exit(app.exec_())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
