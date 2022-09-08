b388c166646b133965ba70ab7784a2f093a78a97
Pierre Gillet <prrgillet@gmail.com>
2022-09-08 16:35:02 +0200

    [WIP] essai d'ajout d'un widget dans le tabwidget

--- bricabrac/main.py
+++ bricabrac/main.py
@@ -21,7 +21,8 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
     win = Ui_MainWindow()
-    win.show()
+ #   win.setupUi(win)
+   # win.show()
 
     sys.exit(app.exec_())
 
